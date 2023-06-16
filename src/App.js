290cfc84e89df6cfbf48a99c7da7071c432c5b5d
alishahi <shahy8227@gmail.com>
2023-06-16 13:08:11 -0700

    ساخت کامپوننت های ورود و ثبت نامو روت بندی صفحات

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Navbar from "./components/navbar/Navbar";
+import {BrowserRouter , Routes , Route} from 'react-router-dom';
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 function App() {
   return (
-   <div>
-     <Navbar />
-   </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -7,13 +8,13 @@ const Navbar = () => {
        <div className="collapse navbar-collapse" id="navbarSupportedContent">
          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
            <li className="nav-item">
-             <a className="nav-link active" aria-current="page" href="#">خانه</a>
+             <Link className="nav-link active" aria-current="page" to='/'>خانه</Link>
            </li>
            <li className="nav-item">
-             <a className="nav-link" href="#">ورود</a>
+             <Link className="nav-link" to="login">ورود</Link>
            </li>
            <li className="nav-item">
-             <a className="nav-link" href="#">ثبت نام</a>
+             <Link className="nav-link" to='/register'>ثبت نام</Link>
            </li>
          </ul>
        
