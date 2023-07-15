fdd24c74c12845d7d72ac3b1f30287b9692805db
alishahi <shahy8227@gmail.com>
2023-07-15 12:30:12 -0700

    دریافت و دخیره اطلاعات کاربر برای ثبت نام

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
-import './auth.css'
+import './auth.css';
+import { useState } from 'react';
+import axios from 'axios';
 function Register() {
+    const [name , setName] = useState('');
+    const[email , setEmail] = useState(''); 
+    const [password , setPassword] = useState('');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const data = {
+            name,email,password
+        }
+        
+        
+    }
   return (
     <div className="auth register">
         <div className="container">
@@ -11,18 +25,27 @@ function Register() {
                             ثبت نام کنید
                         </h3>
                     </div>
-                    <form action="">
+                    <form action="" onSubmit={handleSubmit} >
                         <div className="form-group mt-3">
                             <label htmlFor="" className="text-white mb-2">نام شما</label>
-                            <input type="text" className='form-control' />
+                            <input type="text" className='form-control' 
+                                onChange={(e)=> setName(e.target.value)}
+                                name='name'
+                            />
                         </div>
                         <div className="form-group mt-3">
                             <label htmlFor="" className="text-white mb-2">ایمیل</label>
-                            <input type="text" className='form-control' />
+                            <input type="text" className='form-control' 
+                                onChange={(e)=> setEmail(e.target.value)}
+                                name='email'
+                            />
                         </div>
                         <div className="form-group mt-3">
                             <label htmlFor="" className="text-white mb-2">پسوورد</label>
-                            <input type="text" className='form-control' />
+                            <input type="text" className='form-control' 
+                                onChange={(e)=> setPassword(e.target.value)}
+                                name='password'
+                            />
                         </div>
                         <div className="form-group mt-4">
                             <button type='submit' className='btn btn-success w-100'>ثبت نام</button>
